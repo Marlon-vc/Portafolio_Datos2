0eaa28216ff418afcef7006ea202d0de0c50a575
Marlon Vega <mvegach2013@gmail.com>
2019-08-20 19:46:23 -0600

    Minor changes

--- Tareas/Tarea2/src/Server/ServerLogic.cpp
+++ Tareas/Tarea2/src/Server/ServerLogic.cpp
@@ -1,7 +1,16 @@
-class ServerLogic {
-	public:
-		ServerLogic();
-		char *process(char *data);
-};
+#include "Server.cpp"
+#include <yajl/yajl_parse.h>
+#include <yajl/yajl_gen.h>
 
-class 
+class StructuresLogic: public ServerLogic { };
+
+char *StructuresLogic::ServerLogic::process(char *data) {
+	
+	return "response";
+}
+
+int main() {
+	StructuresLogic *logic = new StructuresLogic();
+	Server *server = new Server(logic);
+	server->start();
+}
